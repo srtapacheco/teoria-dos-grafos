d3608cd8d54289af5aa1a43d8313b3dfaaf6d18d
srtapacheco <karenpacheco16@poli.ufrj.br>
2024-09-25 08:34:46 -0300

    adicionando funcao de busca em grafos bfs e dfs

--- trabalho1/biblioteca_grafos.c
+++ trabalho1/biblioteca_grafos.c
@@ -2,93 +2,155 @@
 #include <stdlib.h>
 #include <string.h>
 
-int compara(const void *a, const void *b) {
-    return (*(int*)a - *(int*)b);
+#define MAX_VERTICES 100
+
+
+int compara(const void *a, const void *b) // Função auxiliar para comparar dois inteiros, usada na ordenação dos graus
+{
+    return (*(int *)a - *(int *)b); // Função de comparação para a função qsort
 }
 
-void cria_matriz_adjacencia(FILE *arquivo, int num_vertices, int **matriz, int *graus) {
+
+void cria_matriz_adjacencia(FILE *arquivo, int num_vertices, int **matriz)
+{
     int v1, v2;
-    while (fscanf(arquivo, "%d %d", &v1, &v2) != EOF) {
+    while (fscanf(arquivo, "%d %d", &v1, &v2) != EOF)
+    {
         matriz[v1 - 1][v2 - 1] = 1;
         matriz[v2 - 1][v1 - 1] = 1;
-        graus[v1 - 1]++;  
-        graus[v2 - 1]++;  
     }
 }
 
-void cria_lista_adjacencia(FILE *arquivo, int num_vertices, int *graus, int **lista) {
-    int v1, v2;
-    while (fscanf(arquivo, "%d %d", &v1, &v2) != EOF) {
-        lista[v1 - 1][graus[v1 - 1]++] = v2 - 1;
-        lista[v2 - 1][graus[v2 - 1]++] = v1 - 1;
+// Implementação da BFS
+void bfs(int **matriz, int num_vertices, int vertice_inicial, FILE *arquivo_saida)
+{
+    int fila[MAX_VERTICES], nivel[MAX_VERTICES], pai[MAX_VERTICES], visitado[MAX_VERTICES];
+    int inicio = 0, fim = 0;
+
+    for (int i = 0; i < num_vertices; i++)
+    {
+        visitado[i] = 0;
+        nivel[i] = -1; 
+        pai[i] = -1;   
+    }
+
+    fila[fim++] = vertice_inicial;
+    visitado[vertice_inicial] = 1;
+    nivel[vertice_inicial] = 0;
+    pai[vertice_inicial] = 0; 
+
+    fprintf(arquivo_saida, "\nBusca em Largura (BFS):\n");
+    while (inicio < fim)
+    {
+        int atual = fila[inicio++];
+
+        for (int i = 0; i < num_vertices; i++)
+        {
+            if (matriz[atual][i] && !visitado[i])
+            {
+                fila[fim++] = i;
+                visitado[i] = 1;
+                nivel[i] = nivel[atual] + 1;
+                pai[i] = atual + 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < num_vertices; i++)
+    {
+        fprintf(arquivo_saida, "Vértice: %d, Pai: %d, Nível: %d\n", i + 1, pai[i], nivel[i]);
     }
 }
 
-void processa_arquivo_grafo(const char *nome_arquivo, const char *representacao) {
+// Implementação da DFS
+void dfs_recursiva(int **matriz, int vertice, int *visitado, int *nivel, int *pai, int profundidade, FILE *arquivo_saida, int num_vertices)
+{
+    visitado[vertice] = 1;
+    nivel[vertice] = profundidade;
+
+    for (int i = 0; i < num_vertices; i++)
+    {
+        if (matriz[vertice][i] && !visitado[i])
+        {
+            pai[i] = vertice + 1;
+            dfs_recursiva(matriz, i, visitado, nivel, pai, profundidade + 1, arquivo_saida, num_vertices);
+        }
+    }
+}
+
+void dfs(int **matriz, int num_vertices, int vertice_inicial, FILE *arquivo_saida)
+{
+    int visitado[MAX_VERTICES], nivel[MAX_VERTICES], pai[MAX_VERTICES];
+
+    for (int i = 0; i < num_vertices; i++)
+    {
+        visitado[i] = 0;
+        nivel[i] = -1;
+        pai[i] = -1;
+    }
+
+    pai[vertice_inicial] = 0;
+    dfs_recursiva(matriz, vertice_inicial, visitado, nivel, pai, 0, arquivo_saida, num_vertices);
+
+    fprintf(arquivo_saida, "\nBusca em Profundidade (DFS):\n");
+    for (int i = 0; i < num_vertices; i++)
+    {
+        fprintf(arquivo_saida, "Vértice: %d, Pai: %d, Nível: %d\n", i + 1, pai[i], nivel[i]);
+    }
+}
+
+void processa_arquivo_grafo(const char *nome_arquivo, const char *representacao, int vertice_inicial)
+{
     FILE *arquivo = fopen(nome_arquivo, "r");
-    if (arquivo == NULL) {
+    if (arquivo == NULL)
+    {
         printf("Erro ao abrir o arquivo: %s\n", nome_arquivo);
         return;
     }
 
     FILE *arquivo_saida = fopen("saida_grafo.txt", "w");
-    if (arquivo_saida == NULL) {
+    if (arquivo_saida == NULL)
+    {
         printf("Erro ao criar o arquivo de saída.\n");
         fclose(arquivo);
         return;
     }
 
-    int num_vertices;
-    fscanf(arquivo, "%d", &num_vertices); 
-    int graus[num_vertices]; 
-    for (int i = 0; i < num_vertices; i++) {
-        graus[i] = 0;  
+    int num_vertices, v1, v2;
+    fscanf(arquivo, "%d", &num_vertices);
+    int graus[num_vertices];
+    for (int i = 0; i < num_vertices; i++)
+    {
+        graus[i] = 0;
     }
 
     int num_arestas = 0;
-    rewind(arquivo); // Volta ao início para reler o número de vértices
-    fscanf(arquivo, "%d", &num_vertices); // Rele o número de vértices
-    
-    // Escolhe a representação
-    if (strcmp(representacao, "matriz") == 0) {
-        // Aloca e inicializa a matriz de adjacência
-        int **matriz = (int **)malloc(num_vertices * sizeof(int *));
-        for (int i = 0; i < num_vertices; i++) {
-            matriz[i] = (int *)calloc(num_vertices, sizeof(int)); // Inicializa com 0
-        }
 
-        // Preenche a matriz de adjacência e calcula graus
-        cria_matriz_adjacencia(arquivo, num_vertices, matriz, graus);
-
-        // Libera a memória da matriz
-        for (int i = 0; i < num_vertices; i++) {
-            free(matriz[i]);
-        }
-        free(matriz);
-
-    } else if (strcmp(representacao, "lista") == 0) {
-        // Inicializa a lista de adjacência
-        int **lista = (int **)malloc(num_vertices * sizeof(int *));
-        for (int i = 0; i < num_vertices; i++) {
-            lista[i] = (int *)malloc(num_vertices * sizeof(int)); // Aloca espaço suficiente
-        }
-
-        // Preenche a lista de adjacência e calcula graus
-        cria_lista_adjacencia(arquivo, num_vertices, graus, lista);
-
-        // Libera a memória da lista
-        for (int i = 0; i < num_vertices; i++) {
-            free(lista[i]);
-        }
-        free(lista);
+    int **matriz = (int **)malloc(num_vertices * sizeof(int *));
+    for (int i = 0; i < num_vertices; i++)
+    {
+        matriz[i] = (int *)calloc(num_vertices, sizeof(int));
     }
 
-    // Calcula as estatísticas dos graus
+    cria_matriz_adjacencia(arquivo, num_vertices, matriz);
+
+    rewind(arquivo);
+    fscanf(arquivo, "%d", &num_vertices);
+    while (fscanf(arquivo, "%d %d", &v1, &v2) != EOF)
+    {
+        graus[v1 - 1]++;
+        graus[v2 - 1]++;
+        num_arestas++;
+    }
+
     int grau_min = graus[0], grau_max = graus[0];
     double soma_graus = 0.0;
-    for (int i = 0; i < num_vertices; i++) {
-        if (graus[i] < grau_min) grau_min = graus[i];
-        if (graus[i] > grau_max) grau_max = graus[i];
+    for (int i = 0; i < num_vertices; i++)
+    {
+        if (graus[i] < grau_min)
+            grau_min = graus[i];
+        if (graus[i] > grau_max)
+            grau_max = graus[i];
         soma_graus += graus[i];
     }
     double grau_medio = soma_graus / num_vertices;
@@ -96,13 +158,15 @@ void processa_arquivo_grafo(const char *nome_arquivo, const char *representacao)
     qsort(graus, num_vertices, sizeof(int), compara);
 
     double mediana_grau;
-    if (num_vertices % 2 == 0) {
+    if (num_vertices % 2 == 0)
+    {
         mediana_grau = (graus[num_vertices / 2 - 1] + graus[num_vertices / 2]) / 2.0;
-    } else {
+    }
+    else
+    {
         mediana_grau = graus[num_vertices / 2];
     }
 
-    // Imprime as estatísticas
     fprintf(arquivo_saida, "Número de vértices: %d\n", num_vertices);
     fprintf(arquivo_saida, "Número de arestas: %d\n", num_arestas);
     fprintf(arquivo_saida, "Grau mínimo: %d\n", grau_min);
@@ -110,20 +174,34 @@ void processa_arquivo_grafo(const char *nome_arquivo, const char *representacao)
     fprintf(arquivo_saida, "Grau médio: %.2f\n", grau_medio);
     fprintf(arquivo_saida, "Mediana de grau: %.2f\n", mediana_grau);
 
+    bfs(matriz, num_vertices, vertice_inicial - 1, arquivo_saida);
+    dfs(matriz, num_vertices, vertice_inicial - 1, arquivo_saida);
+
+    
+    for (int i = 0; i < num_vertices; i++)
+    {
+        free(matriz[i]);
+    }
+    free(matriz); // Libera a memória da matriz
+
     fclose(arquivo);
     fclose(arquivo_saida);
 
-    printf("Dados do grafo foram processados e gravados no arquivo saida_grafo.txt\n");
+    printf("Dados do grafo processados e gravados no arquivo saida_grafo.txt\n");
 }
 
-int main(int argc, char *argv[]) {
-    if (argc < 3) {
-        printf("Uso: %s <arquivo_grafo> <representacao (matriz|lista)>\n", argv[0]);
+int main(int argc, char *argv[])
+{
+    if (argc < 4)
+    {
+        printf("Uso: %s <arquivo_grafo> <representacao (matriz|lista)> <vertice_inicial>\n", argv[0]);
         return 1;
     }
 
     const char *nome_arquivo = argv[1];
-    const char *representacao = argv[2]; // Escolha da representação: matriz ou lista
-    processa_arquivo_grafo(nome_arquivo, representacao);  
+    const char *representacao = argv[2];
+    int vertice_inicial = atoi(argv[3]);
+
+    processa_arquivo_grafo(nome_arquivo, representacao, vertice_inicial);
     return 0;
 }
